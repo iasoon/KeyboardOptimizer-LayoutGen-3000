5f93c842fe846415544f46d45cbe501f37c63ff9
Ilion Beyst <ilion.beyst@gmail.com>
2017-04-30 14:59:48 +0200

    remove unused model code

--- src/model/language.rs
+++
@@ -1,8 +0,0 @@
-use model::TokenId;
-use utils::SeqTable;
-
-pub type FreqTable = SeqTable<TokenId, f64>;
-
-pub struct Language {
-    pub freqs: Vec<FreqTable>,
-}
--- src/model/lt_conf.rs
+++
@@ -1,7 +0,0 @@
-use model::{Groups, GroupId, KeyId};
-use utils::LookupTable;
-
-pub struct LtConf {
-    pub groups: Groups,
-    pub key_masks: LookupTable<(GroupId, KeyId), bool>,
-}
