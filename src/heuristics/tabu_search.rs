4b5ec3e555c8c70978d174be1b1c5b8c77fddd12
Ilion Beyst <ilion.beyst@gmail.com>
2017-05-21 22:53:33 +0200

    add basic tabu search

--- src/heuristics/tabu_search.rs
+++ src/heuristics/tabu_search.rs
@@ -7,9 +7,16 @@ use heuristics::{Walker, Traverser};
 use rand::{thread_rng, Rng};
 
 pub struct TabuSearcher<'a> {
-    layout: Layout<'a>,
-    evaluator: &'a Evaluator,
     traverser: Traverser<'a>,
+    evaluator: &'a Evaluator,
+    tabu: LookupTable<Assignment, usize>,
+    iteration: usize,
+
+    layout: Layout<'a>,
+    score: f64,
+
+    best: Layout<'a>,
+    best_score: f64,
 }
 
 #[derive(Debug)]
@@ -19,74 +26,79 @@ struct ScoredAlteration {
 }
 impl<'a> TabuSearcher<'a> {
     pub fn new(layout: Layout<'a>, evaluator: &'a Evaluator) -> Self {
+        let score = evaluator.evaluate(&layout.group_map);
         TabuSearcher {
+            tabu: LookupTable::new(AssignmentData::new(layout.kb_def), 0),
+            iteration: 0,
             traverser: Traverser::new(&layout, evaluator),
-            layout: layout,
             evaluator: evaluator,
+
+            best: layout.clone(),
+            best_score: score,
+
+            layout: layout,
+            score: score,
+
         }
     }
 
-    fn alter(&mut self, assignments: &[Assignment]) {
-        self.traverser.update(assignments);
-        for &assignment in assignments.iter() {
+    fn do_move(&mut self, alteration: ScoredAlteration) {
+        self.score += alteration.delta;
+        self.traverser.update(alteration.assignments.as_slice());
+        for &assignment in alteration.assignments.iter() {
             self.layout.assign(assignment);
         }
     }
 
     fn best_move(&mut self) -> ScoredAlteration {
+        let iteration = self.iteration;
+        let tabu = &self.tabu;
         let traverser = &mut self.traverser;
+
         self.layout
             .moves()
-            .map(|assignments| {
-                let delta = traverser.score_assignments(assignments.as_slice());
-                let check = traverser.walker.delta(assignments.as_slice());
-                let tol = (10.0 as f64).powi(-12);
-                if (delta - check).abs() > tol {
-                    println!("ERROR: expected {} but was {}, diff: {}", check, delta, check - delta);
-                }
-                ScoredAlteration {
-                    assignments: assignments,
-                    delta: delta,
+            .filter_map(|assignments| {
+                if assignments.iter().any(|&a| tabu[a] >= iteration) {
+                    // move is tabu
+                    None
+                } else {
+                    Some(ScoredAlteration {
+                        delta: traverser.score_assignments(assignments.as_slice()),
+                        assignments: assignments,
+                    })
                 }
             })
             .min_by(|ref a, ref b| a.delta.partial_cmp(&b.delta).unwrap())
             .unwrap()
     }
 
-    pub fn bench(&mut self) {
-        for _ in 0..100 {
-            let moves = {
-                let traverser = &mut self.traverser;
-                self.layout.moves().map(|assignments| {
-                    ScoredAlteration {
-                        delta: traverser.score_assignments(assignments.as_slice()),
-                        assignments: assignments,
-                    }
-                }).collect::<Vec<_>>()
-            };
-
-            let min = moves.iter().min_by(|ref a, ref b| a.delta.partial_cmp(&b.delta).unwrap()).unwrap();
-            println!("best move: {}", min.delta);
-
-            let mv = thread_rng().choose(moves.as_slice()).unwrap();
-            self.alter(mv.assignments.as_slice());
-            self.layout.print();
-        }
-    }
-
-    pub fn climb(&mut self) {
-        let mut iter = 0;
-        loop {
-            iter += 1;
-            println!("iteration {}", iter);
+    pub fn search(&mut self) {
+        let tol = 10f64.powi(-16);
+        while self.iteration < 50_000 {
+            self.iteration += 1;
             let mv = self.best_move();
-            if mv.delta >= 0.0 {
-                println!("best move was {}", mv.delta);
-                return;
+            //println!("{}: {:?}", self.iteration, mv);
+            if mv.delta >= -tol {
+                //println!("iteration {}", self.iteration);
+                //println!("OPTIMUM: {}", self.evaluator.evaluate(&self.layout.group_map));
+                //self.layout.print();
+                for &assignment in mv.assignments.iter() {
+                    let inv = self.traverser.walker.inverse(assignment);
+                    self.tabu[inv] = self.iteration + 25;
+                }
             }
-            self.alter(mv.assignments.as_slice());
-            self.layout.print();
-            println!("score: {}", self.evaluator.evaluate(&self.layout.group_map));
+
+            self.do_move(mv);
+            if self.score + tol < self.best_score {
+                self.best_score = self.score;
+                self.best = self.layout.clone();
+                println!("iteration {}: {}", self.iteration, self.best_score);
+                self.best.print();
+            }
+
         }
+
+        println!("best: {}", self.best_score);
+        self.best.print();
     }
 }
