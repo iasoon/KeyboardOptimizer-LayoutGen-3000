ecd321477ff3fce6a9b17a4a486ce80cba23443e
Ilion Beyst <ilion.beyst@gmail.com>
2017-06-19 23:43:59 +0200

    parse groups

--- src/data/kb_def.rs
+++ src/data/kb_def.rs
@@ -1,9 +1,11 @@
-use cat::{Domain, FiniteDomain, Table};
+use cat::{Domain, FiniteDomain, Table, Num};
 use data::types::*;
 
 pub struct KbDef {
     pub keys: Table<Key, String>,
     pub layers: Table<Layer, String>,
     pub tokens: Table<Token, String>,
-}
 
+    pub frees: Table<Free, Num<Token>>,
+    pub locks: Table<Lock, Table<Layer, Option<Num<Token>>>>,
+}
--- src/data/parser.rs
+++ src/data/parser.rs
@@ -3,8 +3,12 @@ use std::fs::File;
 use std::path::Path;
 use byteorder::{ReadBytesExt, NetworkEndian};
 
-use cat::{FiniteDomain, Table};
+use cat::*;
+use cat::ops::*;
 use data::KbDef;
+use data::types::*;
+
+// A quick and dirty config parser will do for now
 
 error_chain! {
     foreign_links {
@@ -65,17 +69,96 @@ fn read_token_set<R, D>(stream: &mut R) -> Result<Table<D, String>>
     return Ok(Table::from_vec(elems));
 }
 
-fn read_config<R>(stream: &mut R) -> Result<KbDef>
+fn read_num<R, D>(stream: &mut R, count: Count<D>) -> Result<Num<D>>
+    where D: FiniteDomain,
+          R: Read
+{
+    let num = try!(read_u16(stream)) as usize;
+    if num < count.as_usize() {
+        Ok(internal::to_num(num))
+    } else {
+        Err("id out of range".into())
+    }
+
+}
+
+fn read_count<R>(stream: &mut R) -> Result<usize>
+    where R: Read
+{
+    let num = try!(read_u16(stream));
+    return Ok(num as usize);
+}
+
+// Quick and dirty...
+// eww.
+fn read_kb_def<R>(stream: &mut R) -> Result<KbDef>
     where R: Read
 {
     let keys = read_token_set(stream)?;
     let layers = read_token_set(stream)?;
     let tokens = read_token_set(stream)?;
+
+    let mut assignments = Vec::new();
+    // Read frees
+    let num_frees = try!(read_count(stream));
+    let mut frees = Vec::with_capacity(num_frees);
+    for free_num in 0..num_frees {
+        // read free data
+        let token_num = try!(read_num(stream, tokens.count()));
+        frees.push(token_num);
+
+        // read mask
+        let num_locs = try!(read_count(stream));
+        for _ in 0..num_locs {
+            let loc_num = try!(read_num(stream, LocNum {
+                key_count: keys.count(),
+                layer_count: layers.count(),
+            }.count()));
+            assignments.push(Assignment::Free {
+                free_num: internal::to_num(free_num),
+                loc_num: loc_num,
+            })
+        }
+    }
+
+    // Read locks
+    let num_locks = try!(read_count(stream));
+    let mut locks = Vec::with_capacity(num_locks);
+    for lock_num in 0..num_locks {
+        let mut layer_map = layers.map(|_| None);
+        let num_entries = try!(read_u16(stream)) as usize;
+        for _ in 0..num_entries {
+            let layer_num = try!(read_num(stream, layers.count()));
+            let token_num = try!(read_num(stream, tokens.count()));
+            *layer_map.get_mut(layer_num) = Some(token_num);
+        }
+        locks.push(layer_map);
+
+        // read mask
+        let num_keys = try!(read_count(stream));
+        for _ in 0..num_keys {
+            let key_num = try!(read_num(stream, keys.count()));
+            assignments.push(Assignment::Lock {
+                lock_num: internal::to_num(lock_num),
+                key_num: key_num,
+            });
+        }
+    }
+
     Ok(KbDef {
         keys: keys,
         layers: layers,
         tokens: tokens,
+
+        frees: Table::from_vec(frees),
+        locks: Table::from_vec(locks),
     })
+}
+
+fn read_config<R>(stream: &mut R) -> Result<KbDef>
+    where R: Read
+{
+    unimplemented!()
 }
 
 pub fn parse(path: &Path) -> Result<KbDef>
