47d16a064c250078bbb22c68f1a4ef72ee3367e4
Ilion Beyst <ilion.beyst@gmail.com>
2017-07-02 21:11:22 +0200

    start working on move_gen

--- src/layout/move_gen.rs
+++ src/layout/move_gen.rs
@@ -3,25 +3,80 @@ use cat::*;
 
 use layout::*;
 
+pub struct MoveGen<'a> {
+    kb_def: &'a KbDef,
+    keymap: &'a Keymap,
+    assignment_used: ElemTable<Assignment, AssignmentNum, bool>,
+}
+
+impl<'a> MoveGen<'a> {
+    fn next_assignment(&mut self) -> Option<Assignment> {
+        unimplemented!()
+    }
+
+    fn next_move(&mut self) -> Option<Vec<Assignment>> {
+        while let Some(assignment) = self.next_assignment() {
+            let mut builder = self.move_builder();
+            builder.assignments.push(assignment);
+            builder.resolve();
+            if builder.move_allowed() {
+                return Some(builder.assignments);
+            }
+        }
+        return None;
+    }
+
+    fn move_builder<'b>(&'b mut self) -> MoveBuilder<'b> {
+        MoveBuilder {
+            kb_def: self.kb_def,
+            keymap: self.keymap,
+            assignment_used: &mut self.assignment_used,
+            assignments: Vec::with_capacity(
+                // an assignment swaps tokens between two keys, and each key has
+                // at most #layers tokens. Thus, 2*#layers is a (liberal) upper
+                // bound for the number of assignments in a move.
+                2 * self.kb_def.layers.count().as_usize()
+            ),
+        }
+    }
+}
+
+impl<'a> Iterator for MoveGen<'a> {
+    type Item = Vec<Assignment>;
+
+    fn next(&mut self) -> Option<Vec<Assignment>> {
+        self.next_move()
+    }
+}
+
+/// Careful: this MoveBuilder does not always yield correct moves.
+/// The moves it produces will however always be correct when
+/// - it is seeded with a lock
+/// - all lock-assignments are already marked as used
+/// In both these cases, the caveat in get_assignment does not occur.
 pub struct MoveBuilder<'a> {
     kb_def: &'a KbDef,
 
     keymap: &'a Keymap,
-    token_map: &'a TokenMap,
 
     assignment_used: &'a mut ElemTable<Assignment, AssignmentNum, bool>,
     assignments: Vec<Assignment>,
 }
 
 impl<'a> MoveBuilder<'a> {
-    fn build(mut self) -> Vec<Assignment> {
+    fn move_allowed(&self) -> bool {
+        self.assignments.iter().all(|&assignment| {
+            self.kb_def.assignment_map.get(assignment).is_some()
+        })
+    }
+
+    fn resolve(&mut self) {
         let mut pos = 0;
         while pos < self.assignments.len() {
             let assignment = self.assignments[pos];
             self.assign(self.kb_def, assignment);
             pos += 1;
         }
-        return self.assignments;
     }
 
     fn queue_assignment(&mut self, assignment: Assignment) {
@@ -29,7 +84,10 @@ impl<'a> MoveBuilder<'a> {
         self.assignments.push(assignment);
     }
 
-    /// Get assignment that will move token_num to loc_num
+    /// Get assignment that will move token_num to loc_num.
+    /// When swapping a free with a lock, free tokens should never change layer,
+    /// as this opens doors to all kinds of difficult situations.
+    /// This method assumes that this never happens.
     fn get_assignment(&self, token_num: Num<Token>, loc_num: Num<Loc>)
                       -> Assignment
     {
