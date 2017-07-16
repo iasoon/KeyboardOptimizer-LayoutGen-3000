0b0a38b589227fc1411b861dd906e0f12890efc6
Ilion Beyst <ilion.beyst@gmail.com>
2017-07-16 16:48:35 +0200

    generalize NGramEval

--- src/eval/ngram_eval.rs
+++ src/eval/ngram_eval.rs
@@ -1,21 +1,35 @@
+use std::marker::PhantomData;
+
 use data::*;
 use cat::*;
 
-pub struct NGram;
-
-impl Domain for NGram {
-    type Type = Vec<Num<Group>>;
+pub struct NGram<T> {
+    phantom: PhantomData<T>,
 }
 
-impl FiniteDomain for NGram {}
-
-pub struct NGrams {
-    pub elements: SeqTable<NGram, Num<Group>>,
-    pub freqs: Table<NGram, f64>,
+impl<T> Domain for NGram<T>
+    where T: FiniteDomain
+{
+    type Type = Vec<Num<T>>;
 }
 
-impl NGrams {
-    pub fn eval<'e>(&self, cost: NGramCost<'e, Group, Key>) -> f64 {
+impl<T> FiniteDomain for NGram<T>
+    where T: FiniteDomain
+{}
+
+pub struct NGrams<T>
+    where T: FiniteDomain
+{
+    pub elements: SeqTable<NGram<T>, Num<T>>,
+    pub freqs: Table<NGram<T>, f64>,
+}
+
+impl<T> NGrams<T>
+    where T: FiniteDomain
+{
+    pub fn eval<'e, P>(&self, cost: NGramCost<'e, T, P>) -> f64
+        where P: FiniteDomain
+    {
         self.elements.enumerate().map(|(seq_num, seq)| {
             cost.apply(seq) * self.freqs.get(seq_num)
         }).sum()
@@ -24,21 +38,27 @@ impl NGrams {
 
 pub type PathCost<T> = Composed<SeqNum<T>, Table<Seq<T>, f64>>;
 
-pub struct NGramEval {
-    pub ngrams: NGrams,
-    pub costs: PathCost<Key>,
+pub struct NGramEval<T, P>
+    where T: FiniteDomain,
+          P: FiniteDomain
+{
+    pub ngrams: NGrams<T>,
+    pub costs: PathCost<P>,
 }
 
-impl NGramEval {
-    fn ngram_cost<'e>(&'e self, group_map: &'e Table<Group, Num<Key>>) -> NGramCost<'e, Group, Key> {
+impl<T, P> NGramEval<T, P>
+    where T: FiniteDomain,
+          P: FiniteDomain
+{
+    fn ngram_cost<'e>(&'e self, mapping: &'e Table<T, Num<P>>) -> NGramCost<'e, T, P> {
         NGramCost {
-            mapping: group_map,
+            mapping: mapping,
             path_cost: &self.costs,
         }
     }
 
-    fn eval(&self, group_map: &Table<Group, Num<Key>>) -> f64 {
-        self.ngrams.eval(self.ngram_cost(group_map))
+    pub fn eval(&self, mapping: &Table<T, Num<P>>) -> f64 {
+        self.ngrams.eval(self.ngram_cost(mapping))
     }
 }
 
