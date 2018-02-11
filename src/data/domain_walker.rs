a8fc22ec955f3d43e286459458e23e19f3f597c2
Ilion Beyst <ilion.beyst@gmail.com>
2018-02-11 20:53:28 +0100

    simple implementation of domain walker

---
+++ src/data/domain_walker.rs
@@ -0,0 +1,44 @@
+use super::RestrictedRange;
+use super::types::*;
+
+use cat::*;
+
+pub struct DomainWalker<'d> {
+    domain: &'d Domain,
+    ranges: Table<Key, RestrictedRange>,
+}
+
+impl<'d> DomainWalker<'d> {
+    pub fn new(domain: &'d Domain) -> Self {
+        let ranges = domain.keys.map_nums(|_| {
+            RestrictedRange::new(domain.values.count())
+        });
+
+        DomainWalker {
+            domain: domain,
+            ranges: ranges,
+        }
+    }
+
+    pub fn range_for<'a>(&'a self, key_num: Num<Key>) -> &'a [Num<Value>] {
+        self.ranges[key_num].accepted()
+    }
+
+    pub fn assign(&mut self, key_num: Num<Key>, value_num: Num<Value>) {
+        for constraint in self.domain.constraints.iter() {
+            if constraint.object == key_num {
+                let restriction = &constraint.restrictor[value_num];
+                self.ranges[constraint.subject].add_restriction(restriction);
+            }
+        }
+    }
+
+    pub fn unassign(&mut self, key_num: Num<Key>, value_num: Num<Value>) {
+        for constraint in self.domain.constraints.iter() {
+            if constraint.object == key_num {
+                let restriction = &constraint.restrictor[value_num];
+                self.ranges[constraint.subject].remove_restriction(restriction);
+            }
+        }
+    }
+}
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,3 +1,7 @@
 mod types;
+mod restricted_range;
+mod domain_walker;
 
 pub use self::types::*;
+pub use self::restricted_range::RestrictedRange;
+pub use self::domain_walker::DomainWalker;
